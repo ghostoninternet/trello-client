bf78abc51a5af16f42d099056d3fd3dd7bddd28c
ghostoninternet <daosyphuc288@gmail.com>
2023-11-02 16:21:42 +0700

    Change from createTheme to extendTheme

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,27 @@ import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
 import HomeIcon from '@mui/icons-material/Home'
 import Typography from '@mui/material/Typography'
+import { useColorScheme } from '@mui/material/styles'
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
 
 function App() {
 
   return (
     <>
+      <ModeToggle />
+      <hr />
       <div>Hello World</div>
       <Typography variant='body2' color="text.secondary">Test typography</Typography>
       <Button variant="text">Text</Button>
