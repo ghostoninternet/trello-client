ce37969fca6c3fbfb4ac6b74b948981336e58e60
ghostoninternet <daosyphuc288@gmail.com>
2023-12-12 07:19:24 +0700

    Add new feature: add new column and card

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react'
+import { isEmpty } from 'lodash'
 import Container from '@mui/material/Container'
 import AppBar from '~/components/AppBar/AppBar'
 import BoardBar from './BoardBar/BoardBar'
 import BoardContent from './BoardContent/BoardContent'
-import { fetchBoardDetailsAPI } from '~/apis'
+import { fetchBoardDetailsAPI, createColumnDetailsAPI, createCardDetailsAPI } from '~/apis'
+import { generatePlaceholderCard } from '~/utils/formatter'
 
 function Board() {
   const [board, setBoard] = useState(null)
@@ -11,14 +13,57 @@ function Board() {
   useEffect(() => {
     const boardId = '65731f08f46c9b40333f4b5c'
     fetchBoardDetailsAPI(boardId).then(board => {
+      board.columns.forEach(column => {
+        if (isEmpty(column.cards)) {
+          column.cards = [generatePlaceholderCard(column)]
+          column.cardOrderIds = [generatePlaceholderCard(column)._id]
+        }
+      })
       setBoard(board)
     })
   }, [])
+
+  // Call API create new column and update board state
+  const createNewColumn = async (newColumnData) => {
+    const createdColumn = await createColumnDetailsAPI({
+      ...newColumnData,
+      boardId: board._id
+    })
+
+    createdColumn.cards = [generatePlaceholderCard(createdColumn)]
+    createdColumn.cardOrderIds = [generatePlaceholderCard(createdColumn)._id]
+
+    // Update board state
+    const newBoard = { ...board }
+    newBoard.columns.push(createdColumn)
+    newBoard.columnOrderIds.push(createdColumn._id)
+    setBoard(newBoard)
+  }
+
+  // Call API create new card and update board state
+  const createNewCard = async (newCardData) => {
+    const createdCard = await createCardDetailsAPI({
+      ...newCardData,
+      boardId: board._id
+    })
+    // Update board state
+    const newBoard = { ...board }
+    const columnToUpdate = newBoard.columns.find(column => column._id === createdCard.columnId)
+    if (columnToUpdate) {
+      columnToUpdate.cards.push(createdCard)
+      columnToUpdate.cardOrderIds.push((createdCard._id))
+    }
+    setBoard(newBoard)
+  }
   return (
     <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
       <AppBar />
       <BoardBar board={board} />
-      <BoardContent board={board} />
+      <BoardContent
+        board={board}
+        createNewColumn={createNewColumn}
+        createNewCard={createNewCard}
+      />
     </Container>
   )
 }
